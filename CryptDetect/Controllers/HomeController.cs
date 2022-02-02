8ae4ec08c31a9656fc75ceeed27e641c0f1a5a6a
Nathan Henderson <nathhendo@hotmail.com>
2022-02-02 19:20:40 +1100

    Added Test File

--- CryptDetect/Controllers/HomeController.cs
+++ CryptDetect/Controllers/HomeController.cs
@@ -38,6 +38,11 @@ namespace CryptDetect.Controllers
             return View();
         }
 
+        public IActionResult Test()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
